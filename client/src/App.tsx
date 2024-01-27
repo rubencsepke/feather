71ba35b9fcc354bda1e92479f97992610509696a
Ruben Csepke <ruben.csepke@gmail.com>
2024-01-27 23:02:09 +0100

    refactor: remove unused imports, use post status, update Post model with User

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,14 @@
 import './App.css'
 import PostsComponent from './components/posts/posts.component'
 import FormComponent from './components/form/form.component'
-import { QueryClient, QueryClientProvider } from 'react-query'
 
 function App() {
-  const queryClient = new QueryClient()
 
   return (
-    <QueryClientProvider client={queryClient}>
+    <>
       <FormComponent />
       <PostsComponent />
-    </QueryClientProvider>
+    </>
   )
 }
 
--- client/src/components/posts/posts.component.tsx
+++ client/src/components/posts/posts.component.tsx
@@ -2,22 +2,35 @@ import styles from './posts.component.module.scss';
 import {useGetPosts} from "../../hooks/usePosts.hook.ts";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
+import {timeAgo} from "../../utils/calc-time-ago.ts";
 
 function PostsComponent() {
-  const { posts } = useGetPosts();
+  const { posts, error, isLoading } = useGetPosts();
+
+  if(isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if(error) {
+    return <div>Error</div>
+  }
+
+  if(!posts) {
+    return <div>No posted data!</div>;
+  }
 
   return (
     <div className={styles.container}>
-      {posts.data && posts.data.map((item, index) => (
+      {posts.map((post, index) => (
         <div key={index} className={styles.postItem}>
           <img className={styles.authorProfileImage} src="https://placehold.co/50x50" alt="Placeholder user image" />
           <div className={styles.authorContent}>
             <div className={styles.author}>
-              <strong>{item.post_author}</strong>
-              <time>30m</time>
+              <strong>{post.post_author.username}</strong>
+              <time>{timeAgo(post.date)}</time>
             </div>
             <div className={styles.post}>
-              <p>{item.content}</p>
+              <p>{post.content}</p>
             </div>
             <div className={styles.actions}>
               <button title="Like" className={styles.actionButton}><FontAwesomeIcon icon={icon({name: 'heart', style: 'regular'})} /></button>
